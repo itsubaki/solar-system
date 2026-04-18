92ec8257bdc1e2bd3d8915d25a6f99a14f736d70
itsubaki <1759459+itsubaki@users.noreply.github.com>
2026-04-18 20:32:12 +0900

    Update size

--- lib/planet-data.ts
+++ lib/planet-data.ts
@@ -220,7 +220,7 @@ export const SUN_DATA = {
 export const PLANETS: PlanetData[] = [
     {
         name: "Mercury",
-        radius: 2439.7 / 71492,
+        radius: (2439.7 / 71492) * 3,
         distance: 3.871,
         orbitalPeriod: 88,
         rotationPeriod: 58.6,
@@ -229,7 +229,7 @@ export const PLANETS: PlanetData[] = [
     },
     {
         name: "Venus",
-        radius: 6051.8 / 71492,
+        radius: (6051.8 / 71492) * 3,
         distance: 7.233,
         orbitalPeriod: 225,
         rotationPeriod: 243,
@@ -238,7 +238,7 @@ export const PLANETS: PlanetData[] = [
     },
     {
         name: "Earth",
-        radius: 6371 / 71492,
+        radius: (6371 / 71492) * 3,
         distance: 10.0,
         orbitalPeriod: 365,
         rotationPeriod: 1,
@@ -246,7 +246,7 @@ export const PLANETS: PlanetData[] = [
         satellites: [
             {
                 name: "Moon",
-                radius: 1737.4 / 71492,
+                radius: (1737.4 / 71492) * 3,
                 distance: 0.8,
                 orbitalPeriod: 27,
                 color: "#C4C4C4"
@@ -256,7 +256,7 @@ export const PLANETS: PlanetData[] = [
     },
     {
         name: "Mars",
-        radius: 3389.5 / 71492,
+        radius: (3389.5 / 71492) * 3,
         distance: 15.237,
         orbitalPeriod: 687,
         rotationPeriod: 1.03,
@@ -264,14 +264,14 @@ export const PLANETS: PlanetData[] = [
         satellites: [
             {
                 name: "Phobos",
-                radius: 11.267 / 71492,
+                radius: (11.267 / 71492) * 3,
                 distance: 0.5,
                 orbitalPeriod: 0.3,
                 color: "#8B7355"
             },
             {
                 name: "Deimos",
-                radius: 6.2 / 71492,
+                radius: (6.2 / 71492) * 3,
                 distance: 0.7,
                 orbitalPeriod: 1.3,
                 color: "#8B7355"
@@ -281,7 +281,7 @@ export const PLANETS: PlanetData[] = [
     },
     {
         name: "Jupiter",
-        radius: 1.0,
+        radius: 1.0 * 3,
         distance: 52.026,
         orbitalPeriod: 4333,
         rotationPeriod: 0.41,
@@ -289,28 +289,28 @@ export const PLANETS: PlanetData[] = [
         satellites: [
             {
                 name: "Io",
-                radius: 1821.6 / 71492,
+                radius: (1821.6 / 71492) * 3,
                 distance: 1.5,
                 orbitalPeriod: 1.8,
                 color: "#E6C87A"
             },
             {
                 name: "Europa",
-                radius: 1560.8 / 71492,
+                radius: (1560.8 / 71492) * 3,
                 distance: 1.8,
                 orbitalPeriod: 3.5,
                 color: "#C4B896"
             },
             {
                 name: "Ganymede",
-                radius: 2634.1 / 71492,
+                radius: (2634.1 / 71492) * 3,
                 distance: 2.2,
                 orbitalPeriod: 7.2,
                 color: "#8B7355"
             },
             {
                 name: "Callisto",
-                radius: 2410.3 / 71492,
+                radius: (2410.3 / 71492) * 3,
                 distance: 2.6,
                 orbitalPeriod: 16.7,
                 color: "#5C4033"
@@ -320,42 +320,42 @@ export const PLANETS: PlanetData[] = [
     },
     {
         name: "Saturn",
-        radius: 60268 / 71492,
+        radius: (60268 / 71492) * 3,
         distance: 95.549,
         orbitalPeriod: 10759,
         rotationPeriod: 0.45,
         color: "#F4D59E",
         rings: [
             {
-                innerRadius: (60268 / 71492) * (66900 / 58232),
-                outerRadius: (60268 / 71492) * (74510 / 58232),
+                innerRadius: (60268 / 71492) * (66900 / 58232) * 3,
+                outerRadius: (60268 / 71492) * (74510 / 58232) * 3,
                 color: "#E0D7C6"
             },
             {
-                innerRadius: (60268 / 71492) * (74658 / 58232),
-                outerRadius: (60268 / 71492) * (92000 / 58232),
+                innerRadius: (60268 / 71492) * (74658 / 58232) * 3,
+                outerRadius: (60268 / 71492) * (92000 / 58232) * 3,
                 color: "#E5D7B9"
             },
             {
-                innerRadius: (60268 / 71492) * (92000 / 58232),
-                outerRadius: (60268 / 71492) * (117580 / 58232),
+                innerRadius: (60268 / 71492) * (92000 / 58232) * 3,
+                outerRadius: (60268 / 71492) * (117580 / 58232) * 3,
                 color: "#F4D59E"
             },
             {
-                innerRadius: (60268 / 71492) * (122170 / 58232),
-                outerRadius: (60268 / 71492) * (136775 / 58232),
+                innerRadius: (60268 / 71492) * (122170 / 58232) * 3,
+                outerRadius: (60268 / 71492) * (136775 / 58232) * 3,
                 color: "#F4D59E"
             },
             {
-                innerRadius: (60268 / 71492) * (140180 / 58232),
-                outerRadius: (60268 / 71492) * (140680 / 58232),
+                innerRadius: (60268 / 71492) * (140180 / 58232) * 3,
+                outerRadius: (60268 / 71492) * (140680 / 58232) * 3,
                 color: "#E3E3F7"
             },
         ],
         satellites: [
             {
                 name: "Titan",
-                radius: 2574.7 / 71492,
+                radius: (2574.7 / 71492) * 3,
                 distance: 2.5,
                 orbitalPeriod: 16,
                 color: "#E6A243"
@@ -365,50 +365,50 @@ export const PLANETS: PlanetData[] = [
     },
     {
         name: "Uranus",
-        radius: 25362 / 71492,
+        radius: (25362 / 71492) * 3,
         distance: 192.184,
         orbitalPeriod: 30687,
         rotationPeriod: 0.72,
         color: "#B5E3E3",
         rings: [
             {
-                innerRadius: (25362 / 71492) * (51149 / 25362),
-                outerRadius: (25362 / 71492) * (51710 / 25362),
+                innerRadius: (25362 / 71492) * (51149 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (51710 / 25362) * 3,
                 color: "#B5E3E3"
             },
             {
-                innerRadius: (25362 / 71492) * (47176 / 25362),
-                outerRadius: (25362 / 71492) * (47462 / 25362),
+                innerRadius: (25362 / 71492) * (47176 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (47462 / 25362) * 3,
                 color: "#A0C8C8"
             },
             {
-                innerRadius: (25362 / 71492) * (45619 / 25362),
-                outerRadius: (25362 / 71492) * (45929 / 25362),
+                innerRadius: (25362 / 71492) * (45619 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (45929 / 25362) * 3,
                 color: "#7FC7C7"
             },
             {
-                innerRadius: (25362 / 71492) * (44718 / 25362),
-                outerRadius: (25362 / 71492) * (44918 / 25362),
+                innerRadius: (25362 / 71492) * (44718 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (44918 / 25362) * 3,
                 color: "#E3D7F7"
             },
             {
-                innerRadius: (25362 / 71492) * (41837 / 25362),
-                outerRadius: (25362 / 71492) * (42067 / 25362),
+                innerRadius: (25362 / 71492) * (41837 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (42067 / 25362) * 3,
                 color: "#D7F7E3"
             },
             {
-                innerRadius: (25362 / 71492) * (41837 / 25362),
-                outerRadius: (25362 / 71492) * (41939 / 25362),
+                innerRadius: (25362 / 71492) * (41837 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (41939 / 25362) * 3,
                 color: "#F7E3D7"
             },
             {
-                innerRadius: (25362 / 71492) * (41939 / 25362),
-                outerRadius: (25362 / 71492) * (42067 / 25362),
+                innerRadius: (25362 / 71492) * (41939 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (42067 / 25362) * 3,
                 color: "#E3F7D7"
             },
             {
-                innerRadius: (25362 / 71492) * (42067 / 25362),
-                outerRadius: (25362 / 71492) * (42190 / 25362),
+                innerRadius: (25362 / 71492) * (42067 / 25362) * 3,
+                outerRadius: (25362 / 71492) * (42190 / 25362) * 3,
                 color: "#D7E3F7"
             }
         ],
@@ -416,7 +416,7 @@ export const PLANETS: PlanetData[] = [
     },
     {
         name: "Neptune",
-        radius: 24622 / 71492,
+        radius: (24622 / 71492) * 3,
         distance: 301.104,
         orbitalPeriod: 60190,
         rotationPeriod: 0.67,
@@ -424,7 +424,7 @@ export const PLANETS: PlanetData[] = [
         satellites: [
             {
                 name: "Triton",
-                radius: 1353.4 / 71492,
+                radius: (1353.4 / 71492) * 3,
                 distance: 1.2,
                 orbitalPeriod: 5.9,
                 color: "#C4C4C4"
